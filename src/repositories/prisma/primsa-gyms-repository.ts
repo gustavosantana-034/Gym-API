ca2eb25432a1e6fb1f4845c15ce21632b2ddd651
Darkpwd <natanyuri1010@gmail.com>
2025-07-18 10:26:40 -0300

    feat(prisma): create gyms and users repositories

---
+++ src/repositories/prisma/primsa-gyms-repository.ts
@@ -0,0 +1,42 @@
+import { Gym, Prisma } from '@prisma/client'
+import { findManyNearbyParams, GymsRepository } from '../gyms-repositories'
+import { prisma } from '@/lib/prisma'
+
+export class PrismaGymsRepository implements GymsRepository {
+  async findById(id: string) {
+    return await prisma.gym.findUnique({
+      where: {
+        id,
+      },
+    })
+  }
+
+  async findManyNearby({ latitude, longitude }: findManyNearbyParams) {
+    return await prisma.$queryRaw<Gym[]>`
+    
+        SELECT * FROM gyms
+
+        WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+
+    `
+  }
+
+  async searchMany(query: string, page: number) {
+    return await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+    return await prisma.gym.create({
+      data,
+    })
+  }
+}
